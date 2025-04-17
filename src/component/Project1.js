c5bde634eb9e820e0e3bd02275237a608259ac1a
kishan955971 <kishan221314z@gmail.com>
2025-04-17 13:56:50 +0530

    project

--- src/component/Project1.js
+++ src/component/Project1.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import Design1 from './Design1.css';
-function Project1(Props) {
+import './Design1.css';
+
+function Project1() {
     const [N1, setN1] = useState("");
     const [N2, setN2] = useState("");
     const [Result, setResult] = useState("");
-    const [operation, setOperation] = useState("add"); 
+    const [operation, setOperation] = useState("add");
 
     function handleAdd(e) {
         e.preventDefault();
@@ -14,47 +15,44 @@ function Project1(Props) {
 
         switch (operation) {
             case "add":
-                x = num1 + num2;
-                break;
+                 x = num1 + num2;
+             break;
             case "subtract":
-                x = num1 - num2;
-                break;
+                 x = num1 - num2;
+             break;
             case "multiply":
-                x = num1 * num2;
-                break;
+                 x = num1 * num2;
+             break;
             case "divide":
-                x = num2 !== 0 ? num1 / num2 : "Error"; 
-                break;
-            case "remainder":
-                x = num2 !== 0 ? num1 % num2 : "Error";
-                break;
-            default:
-                x = "Invalid";
+                 x = num2 !== 0 ? num1 / num2 : "Error";
+             break;
+            case "remainder": 
+            x = num2 !== 0 ? num1 % num2 : "Error"; 
+            break;
+            default: x = "Invalid";
         }
+
         setResult(x);
     }
+
     return (
-        <div className='col'>
-            <input className='boxsize' placeholder='N1' value={N1} onChange={(e) => setN1(e.target.value)} type="number" /><br />
-            <input className='boxsize' placeholder='N2' value={N2} onChange={(e) => setN2(e.target.value)} type="number" /><br />
-            <input className='boxsize' placeholder='Result' value={Result} readOnly /><br></br><br></br>
-            <input className='submit' type="submit" onClick={handleAdd} />
-             <h2 className='calculate' >Calculate</h2>
-            <p className='col1'>Add</p>
-            <input className='col1' type="radio" name="c" value="add" checked={operation == "add"} onChange={(e) => setOperation(e.target.value)}></input>
+        <div className="container">
+            <h2 className="title">Simple Calculator</h2>
+            <input className="boxsize" placeholder="N1" value={N1} onChange={(e) => setN1(e.target.value)} type="number" />
+            <input className="boxsize" placeholder="N2" value={N2} onChange={(e) => setN2(e.target.value)} type="number" />
+            <input className="boxsize" placeholder="Result" value={Result} readOnly />
 
-            <p className='col1'>Minus</p>
-            <input className='col1' type="radio" name="c" value="subtract" checked={operation == "subtract"} onChange={(e) => setOperation(e.target.value)}></input>
+            <div className="operations">
+                <label><input type="radio" name="op" value="add" checked={operation === "add"} onChange={(e) => setOperation(e.target.value)} /> Add</label>
+                <label><input type="radio" name="op" value="subtract" checked={operation === "subtract"} onChange={(e) => setOperation(e.target.value)} /> Subtract</label>
+                <label><input type="radio" name="op" value="multiply" checked={operation === "multiply"} onChange={(e) => setOperation(e.target.value)} /> Multiply</label>
+                <label><input type="radio" name="op" value="divide" checked={operation === "divide"} onChange={(e) => setOperation(e.target.value)} /> Divide</label>
+                <label><input type="radio" name="op" value="remainder" checked={operation === "remainder"} onChange={(e) => setOperation(e.target.value)} /> Remainder</label>
+            </div>
 
-            <p className='col2'>Multiply</p>
-            <input className='col2' type="radio" name="c" value="multiply" checked={operation == "multiply"} onChange={(e) => setOperation(e.target.value)}></input>
-
-            <p className='col3'>Divide</p>
-            <input className='col3'  type="radio" name="c" value="divide" checked={operation == "divide"} onChange={(e) => setOperation(e.target.value)}></input>
-
-            <p className='col4'>Remainder</p>
-            <input className='col4' type="radio" name="c" value="remainder" checked={operation == "remainder"} onChange={(e) => setOperation(e.target.value)}></input>
+            <button className="submit" onClick={handleAdd}>Calculate</button>
         </div>
     );
 }
+
 export default Project1;
